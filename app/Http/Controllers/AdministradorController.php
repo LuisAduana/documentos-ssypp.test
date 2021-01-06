926f8758041ad098ce25783fb99af3208eb85bcf
LuisAduana <38675549+LuisAduana@users.noreply.github.com>
2021-01-06 15:36:19 -0600

    Implementación Modificar Coordinador

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class AdministradorController extends Controller
 {
@@ -17,15 +18,15 @@ class AdministradorController extends Controller
             'nombres' => 'required|min:1|max:90',
             'apellido_paterno' => 'required|min:1|max:45',
             'apellido_materno' => 'required|min:1|max:45',
-            'estado' => 'required|min:1|max:10',
+            'estado' => 'required|min:1|max:11',
             'num_contacto' => 'required|min:1|max:20',
             'rol_usuario' => 'required|min:1|max:13',
             'num_personal' => 'required|min:10|max:10|unique:profesor'
         ];
 
         $customMessages = [
-            'correo.unique' => 'El correo electrónico :attribute ya ha sido registrado.',
-            'num_personal.unique' => 'El número de personal :attribute ya ha sido registrado.',
+            'correo.unique' => 'El correo electrónico ya ha sido registrado.',
+            'num_personal.unique' => 'El número de personal ya ha sido registrado.',
         ];
 
         $this->validate($request, $rules, $customMessages);
@@ -50,6 +51,42 @@ class AdministradorController extends Controller
         });
     }
 
+    public function modificarCoordinador(Request $request) {
+        $rules = [
+            'correo' => ['required', 'max:150', 'email', Rule::unique('users')->ignore($request->id)],
+            'num_personal' => ['required', 'min:10', 'max:10', Rule::unique('profesor')->ignore($request->id_profesor)],
+            'nombres' => ['required', 'max:90', 'min:1'],
+            'apellido_paterno' => ['required', 'max:45', 'min:1'],
+            'apellido_materno' => ['required', 'max:45', 'min:1'],
+            'estado' => ['required', 'max:11', 'min:1'],
+            'num_contacto' => ['required', 'max:20', 'min:1'],
+            'rol_usuario' => ['required', 'max:13', 'min:1']
+        ];
+
+        $customMessages = [
+            'correo.unique' => 'El correo electrónico ya ha sido registrado.',
+            'num_personal.unique' => 'El número de personal ya ha sido registrado.',
+        ];
+
+        $this->validate($request, $rules, $customMessages);
+
+        DB::transaction(function () use ($request) {
+            DB::update(
+                'update users set correo = ?, password = ?, nombres = ?, 
+                apellido_paterno = ?, apellido_materno = ?, num_contacto = ? where id = ?', [
+                    $request->correo, Hash::make($request->password), $request->nombres,
+                    $request->apellido_paterno, $request->apellido_materno,
+                    $request->num_contacto, $request->id
+                ]
+            );
+
+            DB::update(
+                'update profesor set num_personal = ? where id = ?',
+                [$request->num_personal, $request->id_profesor]
+            );
+        });
+    }
+
     public function activarDesactivarCoordinador(Request $request) {
         DB::update('update users set estado = ? where correo = ?', [$request->estado, $request->correo]);
     }
@@ -60,6 +97,7 @@ class AdministradorController extends Controller
         $coordinadores = array();
         foreach ($query as $coordinador) {
             $localArray = array(
+                'id' => $coordinador->id,
                 'correo' => $coordinador->correo,
                 'nombres' => $coordinador->nombres,
                 'apellido_paterno' => $coordinador->apellido_paterno,
@@ -67,6 +105,7 @@ class AdministradorController extends Controller
                 'estado' => $coordinador->estado,
                 'num_contacto' => $coordinador->num_contacto,
                 'rol_usuario' => $coordinador->rol_usuario,
+                'id_profesor' => $coordinador->profesor->id,
                 'num_personal' => $coordinador->profesor->num_personal
             );
             array_push($coordinadores, $localArray);
@@ -82,7 +121,7 @@ class AdministradorController extends Controller
             'nombres' => ['required', 'max:90', 'min:1'],
             'apellido_paterno' => ['required', 'max:45', 'min:1'],
             'apellido_materno' => ['required', 'max:45', 'min:1'],
-            'estado' => ['required', 'max:10', 'min:1'],
+            'estado' => ['required', 'max:11', 'min:1'],
             'num_contacto' => ['required', 'max:20', 'min:1'],
             'rol_usuario' => ['required', 'max:13', 'min:1']
         ]);
--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::prefix('administrador')->group(function() {
     // Route::post('registro', [AdministradorController::class, 'registro']);
     Route::get('obtener-coordinadores', [AdministradorController::class, 'obtenerCoordinadores']);
     Route::post('registrar-coordinador', [AdministradorController::class, 'registrarCoordinador']);
+    Route::put('modificar-coordinador', [AdministradorController::class, 'modificarCoordinador']);
     Route::post('activar-desactivar-coordinador', [AdministradorController::class, 'activarDesactivarCoordinador']);
 });
 
